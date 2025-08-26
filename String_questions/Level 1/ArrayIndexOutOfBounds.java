abb650319b8545c4b103c35bafd55d9096b3b63c
Ayush Kumar <ayushkumar121105@gmail.com>
2025-08-26 21:54:44 +0530

    question 8

--- String_questions/Level 1/ArrayIndexOutOfBounds.java
+++ String_questions/Level 1/ArrayIndexOutOfBounds.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class ArrayIndexOutOfBounds {
-    
+    static void generateException(String[] names) {
+        System.out.println("Accessing element at index " + names.length + ": " + names[names.length]);
+    }
+    static void handleException(String[] names) {
+        try {
+            System.out.println("Accessing element at index " + names.length + ": " + names[names.length]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught ArrayIndexOutOfBoundsException! Message: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Caught a RuntimeException! Message: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] names = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter name " + (i + 1) + ": ");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println("Generating Exception: ");
+        try {
+            generateException(names);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception occurred in generateException(): " + e);
+        }
+
+        System.out.println("Handling Exception: ");
+        handleException(names);
+
+        sc.close();
+    }
 }
